4831634e370d4bf8c44e2dc809b049fd20461f96
itsHimanshu2001 <himanshuguptaking12@gmail.com>
2022-04-15 15:44:00 +0530

    add start script

--- app.js
+++ app.js
@@ -119,8 +119,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err });
     // res.send('Error Detected!!')
 });
-
-app.listen(3000, () =>{
-    console.log('Serving on port 3000')
+const port = process.env.PORT || 3000;
+app.listen(port, () =>{
+    console.log(`Serving on port ${port}`)
 });
 
